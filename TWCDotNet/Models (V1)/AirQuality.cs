34cbe5fe77b52cc48e62c9917197a969833dfe1a
Everblack <everblackx@outlook.com>
2019-07-08 23:09:32 -0400

    Starting to include lifestyle products - Air Quality is now available.

---
+++ TWCDotNet/Models (V1)/AirQuality.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TWCDotNet.Models__V1_
+{
+    public class AirQuality
+    {
+        [JsonProperty("airquality")]
+        public AirQualityData[] AirQualityInfo { get; set; }
+    }
+    public class AirQualityData
+    {
+        [JsonProperty("class")]
+        public string Class { get; set; }
+        [JsonProperty("key")]
+        public string Key { get; set; }
+        [JsonProperty("area_id")]
+        public string Area_ID { get; set; }
+        [JsonProperty("area_name")]
+        public string Area_Name { get; set; }
+        [JsonProperty("state_cd")]
+        public string State_Code { get; set; }
+        [JsonProperty("country_cd")]
+        public string Country_Code { get; set; }
+        [JsonProperty("latitude")]
+        public double? Latitude { get; set; }
+        [JsonProperty("longitude")]
+        public double? Longitude { get; set; }
+        [JsonProperty("source")]
+        public string Source { get; set; }
+        [JsonProperty("rpt_dt")]
+        public string ReportedDateTime { get; set; }
+        [JsonProperty("valid_time_lap")]
+        public string ValidTime { get; set; }
+        [JsonProperty("process_time_gmt")]
+        public long? ProcessTimeEpoch { get; set; } // in GMT
+        [JsonProperty("action_day_ind")]
+        public string Action_Day { get; set; }
+        [JsonProperty("air_quality_cmnt")]
+        public string AirQualityComment { get; set; }
+        [JsonProperty("data_type")]
+        public string Data_Type { get; set; }
+        [JsonProperty("pollutants")]
+        public Pollutants[] pollutants { get; set; }
+    }
+    public class Pollutants
+    {
+        [JsonProperty("primary_pollutant_ind")]
+        public string Primary_Pollutant_Indicator { get; set; }
+        [JsonProperty("pollutant")]
+        public string Pollutant { get; set; }
+        [JsonProperty("pollutant_phrase")]
+        public string Pollutant_Phrase { get; set; }
+        [JsonProperty("air_quality_idx")]
+        public int? Air_Quality_Index { get; set; }
+        [JsonProperty("air_quality_cat")]
+        public string Air_Quality_Category { get; set; }
+        [JsonProperty("air_quality_cat_idx")]
+        public int? Air_Quality_Category_Index { get; set; }
+    }
+}
--- TWCDotNet/WeatherObject.cs
+++ TWCDotNet/WeatherObject.cs
@@ -15,13 +15,13 @@ namespace TWCDotNet
         public readonly static string _urlV2 = "https://api.weather.com/v2/turbo/{0}?apiKey={1}&format=json&geocode={2}&language=en-US&units=e";
         public readonly static string _urlV3 = "https://api.weather.com/v3/{0}?language=EN&apiKey={1}&geocode={2}&format=json";
 
-
         public enum QueryType
         {
             Observation,
             Hourly,
             Daily,
-            Headlines
+            Headlines,
+            AirQuality
         }
         public enum APIType
         {
@@ -47,6 +47,8 @@ namespace TWCDotNet
             => JsonConvert.DeserializeObject<WeatherObservationV1>(_wc.DownloadString(string.Format(_urlV1, _geocode.Split(',')[0], _geocode.Split(',')[1], ReturnType(QueryType.Observation, APIType.V1), _apiKey)));
         public DailyForecast10Day GetDailyForecast10Day()
             => JsonConvert.DeserializeObject<DailyForecast10Day>(_wc.DownloadString(string.Format(_urlV1, _geocode.Split(',')[0], _geocode.Split(',')[1], ReturnType(QueryType.Daily, APIType.V1), _apiKey)));
+        public AirQuality GetAirQuality()
+            => JsonConvert.DeserializeObject<AirQuality>(_wc.DownloadString(string.Format(_urlV1.Replace("&units=e", string.Empty), _geocode.Split(',')[0], _geocode.Split(',')[1], ReturnType(QueryType.AirQuality, APIType.V1), _apiKey)));
         private string ReturnType (QueryType Type, APIType API)
         {
             string TypeStr = null;
@@ -63,6 +65,9 @@ namespace TWCDotNet
                     break;
                 case QueryType.Headlines:
                     TypeStr = (API == APIType.V3) ? "alerts/headlines" : "alerts/headlines";
+                    break;
+                case QueryType.AirQuality:
+                    TypeStr = "airquality.json";
                     break;
             }
             return TypeStr;
